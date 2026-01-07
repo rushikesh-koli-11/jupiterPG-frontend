82192bc2153fb968fdbc4ec1d47edba53427790f
rushikesh <rushikeshkoli87400@gmail.com>
2026-01-07 10:51:09 +0530

    axios AWS backend Config

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8080",
+  baseURL: "http://51.20.82.201:8080/",
   withCredentials: true, // safe even if not used
 });
 
